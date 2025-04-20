dc5ba985be5ade5a6b4ebb8e8557ce173375fceb
Alfrendo Silalahi <alfrendos72@gmail.com>
2025-04-21 01:02:57 +0700

    learn about spread operator

--- 09-data-stuctures-operators/script.js
+++ 09-data-stuctures-operators/script.js
@@ -59,19 +59,100 @@ const restaurant = {
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`
+    );
+  },
 };
 
-restaurant.orderDelivery({
-  time: '22:30',
-  address: 'Via del Sole, 21',
-  mainIndex: 2,
-  starterIndex: 2,
-});
+// restaurant.orderDelivery({
+//   time: '22:30',
+//   address: 'Via del Sole, 21',
+//   mainIndex: 2,
+//   starterIndex: 2,
+// });
 
-restaurant.orderDelivery({
-  address: 'Sporting Lisbon',
-  starterIndex: 2,
-});
+// restaurant.orderDelivery({
+//   address: 'Sporting Lisbon',
+//   starterIndex: 2,
+// });
+
+///// SPREAD OPERATOR
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+// using spread operator
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+
+// spread operator will not modifying original array
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+// copy array with spread operator
+const mainMenyCopy = [...restaurant.mainMenu];
+
+// join two arrays or more
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(menu);
+
+// iterables: arrays, strings, maps, sets. NOT objects
+const str = 'Alfrendo';
+console.log(...str, 'S.', 'S.');
+
+// const ingredients = [
+//   prompt("Let' make pasta! Ingredient 1?"),
+//   prompt('Ingredient 2?'),
+//   prompt('Ingredient 3?'),
+// ];
+// console.log(ingredients);
+// restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+
+// restaurant.orderPasta(...ingredients);
+
+// objects with spread operator
+const newRestaurant = { ...restaurant, founder: 'Alfrendo Silalahi' };
+console.log(newRestaurant);
+
+// copy object using spread operator
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
+console.log(restaurantCopy.name, restaurant.name);
+
+///// DESTRUCTURING OBJECT
+// const { name, categories, openingHours } = restaurant;
+// console.log({ name, categories, openingHours });
+
+// // rename properties after destructuring object
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+// console.log({ restaurantName, hours, tags });
+
+// // default values
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log({ menu, starters });
+
+// // mutating variables
+// let a = 111;
+// let b = 999;
+
+// const obj = { a: 23, b: 7, c: 14 };
+// ({ a, b } = obj);
+// console.log({ a, b });
+
+// // nested destructuring object
+// const {
+//   fri: { open, close },
+// } = openingHours;
+// console.log({ open, close });
 
 ///// DESTRUCTURING ARRAYS
 // const numbers = [1, 2, 3];
@@ -98,33 +179,3 @@ restaurant.orderDelivery({
 // // default values
 // const [p, q, r, s = 100] = numbers;
 // console.log({ p, q, r, s });
-
-///// DESTRUCTURING OBJECT
-const { name, categories, openingHours } = restaurant;
-console.log({ name, categories, openingHours });
-
-// rename properties after destructuring object
-const {
-  name: restaurantName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
-console.log({ restaurantName, hours, tags });
-
-// default values
-const { menu = [], starterMenu: starters = [] } = restaurant;
-console.log({ menu, starters });
-
-// mutating variables
-let a = 111;
-let b = 999;
-
-const obj = { a: 23, b: 7, c: 14 };
-({ a, b } = obj);
-console.log({ a, b });
-
-// nested destructuring object
-const {
-  fri: { open, close },
-} = openingHours;
-console.log({ open, close });
